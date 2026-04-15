8f6d22e63b2b253ba098061161361f98f1504d8e
lipkerton <petr.petuxof@yandex.ru>
2026-04-15 16:53:36 +0300

    t

--- listener/listener.API/Program.cs
+++ listener/listener.API/Program.cs
@@ -4,33 +4,35 @@ using listener.Application;
 using listener.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-namespace listener.API;
 
-public class Program {
-    static void Main(string[] args)
-    {
-        IConfigurationRoot configuration = new ConfigurationBuilder()
-            .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
-            .AddJsonFile("appsettings.json")
-            .Build();
-        Serilog.Core.Logger _logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(configuration)
-            .CreateLogger();
-        _logger.Information("Starting Service...");
-        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "LOCAL";
+string? basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-        builder.Services.AddSerilog(
-            lc => lc
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-        );
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .SetBasePath(basePath)
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{env}.json", true)
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .Build();
+Serilog.Core.Logger _logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
 
-        builder.Services.AddApplicationService(builder.Configuration);
-        builder.Services.AddInfrastructureServices(builder.Configuration);
+_logger.Information($"Env value: {env}");
+_logger.Information("Starting Service...");
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-        IHost host = builder.Build();
-        host.Run();
-    }
-}
+builder.Services.AddSerilog(
+    lc => lc
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+);
+
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+IHost host = builder.Build();
+host.Run();
